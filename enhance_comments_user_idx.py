40c1c2f41e2a9fac997a656652757825a2c625ac
lovit <soy.lovit@gmail.com>
2019-12-27 01:32:12 +0900

    Print num remains

--- enhance_comments_user_idx.py
+++ enhance_comments_user_idx.py
@@ -88,7 +88,7 @@ def main():
         n_remains = len(indices)
         progress = 100 * (1 - n_remains / n_indices)
         now = str(datetime.now())[:19]
-        print(f'\rscrap comments of a user ({progress:.4}%, -{diff} indices), exceptions={n_exceptions} @ {now}')
+        print(f'\rscrap comments of a user ({progress:.4}%), -{diff} indices, {n_remains} remains, exceptions={n_exceptions} @ {now}')
         time.sleep(sleep)
 
     print('done')
